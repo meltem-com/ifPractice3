30ac6d8356fa1ad954c2c5bd673d8fd5bc8adf4d
meltem ertürk <meltemerturk95@gmail.com>
2024-12-27 02:01:34 +0300

    Add project files.

---
+++ ifPractice3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Girilen sabit bir sayı ile kontrol yapıyoruz
+int sayi = 105; // Sabit olarak 105 kullanıyoruz
+
+// Ekrana sayıyı yazdırıyoruz
+Console.WriteLine($"Girilen sayı: {sayi}");
+
+// İlk kontrol: Sayının 10'a eşit, küçük veya büyük olduğunu kontrol ediyoruz
+if (sayi == 10)
+{
+    Console.WriteLine($"{sayi} girilen sayı 10'a eşittir.");
+}
+else if (sayi < 10)
+{
+    Console.WriteLine($"{sayi} girilen sayı 10'dan küçüktür.");
+}
+else
+{
+    Console.WriteLine($"{sayi} girilen sayı 10'dan büyüktür.");
+}
+
+// Çift veya tek olduğunu kontrol ediyoruz
+if (sayi % 2 == 0)
+{
+    Console.WriteLine($"{sayi} girilen sayı çifttir.");
+}
+else
+{
+    Console.WriteLine($"{sayi} girilen sayı tektir.");
+}
